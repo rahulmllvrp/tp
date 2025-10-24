416330ca3feb3a3a025c143f8b7f2a698af874df
yosonteo <teoyoson@gmail.com>
2025-10-24 16:15:08 +0800

    Create testcases for addp, editp, deletep

---
+++ src/test/java/seedu/address/logic/commands/AddEventCommandTest.java
@@ -0,0 +1,271 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.EventDate;
+import seedu.address.model.event.EventName;
+import seedu.address.model.event.EventTime;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.PersonId;
+
+public class AddEventCommandTest {
+
+    @Test
+    public void constructor_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddEventCommand(null, new HashSet<>()));
+    }
+
+    @Test
+    public void execute_eventAcceptedByModel_addSuccessful() throws Exception {
+        ModelStubAcceptingEventAdded modelStub = new ModelStubAcceptingEventAdded();
+        Event validEvent = new Event(new EventName("Test Event"), new EventDate("01-01-2025"), new EventTime("10:00"));
+
+        CommandResult commandResult = new AddEventCommand(validEvent, new HashSet<>()).execute(modelStub);
+
+        assertEquals(String.format(AddEventCommand.MESSAGE_SUCCESS, Messages.format(validEvent)),
+                commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(validEvent), modelStub.eventsAdded);
+    }
+
+    @Test
+    public void execute_duplicateEvent_throwsCommandException() {
+        Event validEvent = new Event(new EventName("Test Event"), new EventDate("01-01-2025"), new EventTime("10:00"));
+        AddEventCommand addEventCommand = new AddEventCommand(validEvent, new HashSet<>());
+        ModelStubWithEvent modelStub = new ModelStubWithEvent(validEvent);
+
+        assertThrows(CommandException.class, AddEventCommand.MESSAGE_DUPLICATE_EVENT, () -> addEventCommand.execute(modelStub));
+    }
+
+    @Test
+    public void equals() {
+        Event event1 = new Event(new EventName("Event 1"), new EventDate("01-01-2025"), new EventTime("10:00"));
+        Event event2 = new Event(new EventName("Event 2"), new EventDate("02-01-2025"), new EventTime("11:00"));
+        AddEventCommand addEvent1Command = new AddEventCommand(event1, new HashSet<>());
+        AddEventCommand addEvent2Command = new AddEventCommand(event2, new HashSet<>());
+
+        // same object -> returns true
+        assertTrue(addEvent1Command.equals(addEvent1Command));
+
+        // same values -> returns true
+        AddEventCommand addEvent1CommandCopy = new AddEventCommand(event1, new HashSet<>());
+        assertTrue(addEvent1Command.equals(addEvent1CommandCopy));
+
+        // different types -> returns false
+        assertFalse(addEvent1Command.equals(1));
+
+        // null -> returns false
+        assertFalse(addEvent1Command.equals(null));
+
+        // different event -> returns false
+        assertFalse(addEvent1Command.equals(addEvent2Command));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Event event = new Event(new EventName("Test Event"), new EventDate("01-01-2025"), new EventTime("10:00"));
+        AddEventCommand addEventCommand = new AddEventCommand(event, new HashSet<>());
+        String expected = AddEventCommand.class.getCanonicalName() + "{toAdd=" + event + "}";
+        assertEquals(expected, addEventCommand.toString());
+    }
+
+    /**
+     * A default model stub that have all of the methods failing.
+     */
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyAddressBook newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPerson(Person target, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPersonList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void saveStateForUndo(String operationDescription) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean canUndo() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public String undo() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteEvent(Event target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addEvent(Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setEvent(Event target, Event editedEvent) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Event> getFilteredEventList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredEventList(Predicate<Event> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that contains a single event.
+     */
+    private class ModelStubWithEvent extends ModelStub {
+        private final Event event;
+
+        ModelStubWithEvent(Event event) {
+            requireNonNull(event);
+            this.event = event;
+        }
+
+        @Override
+        public boolean hasEvent(Event event) {
+            requireNonNull(event);
+            return this.event.isSameEvent(event);
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            return new AddressBook().getPersonList();
+        }
+    }
+
+    /**
+     * A Model stub that always accept the event being added.
+     */
+    private class ModelStubAcceptingEventAdded extends ModelStub {
+        final ArrayList<Event> eventsAdded = new ArrayList<>();
+
+        @Override
+        public boolean hasEvent(Event event) {
+            requireNonNull(event);
+            return eventsAdded.stream().anyMatch(event::isSameEvent);
+        }
+
+        @Override
+        public void addEvent(Event event) {
+            requireNonNull(event);
+            eventsAdded.add(event);
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            return new AddressBook();
+        }
+
+        @Override
+        public void saveStateForUndo(String operationDescription) {
+            // Do nothing for test
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            return new AddressBook().getPersonList();
+        }
+    }
+}
---
+++ src/test/java/seedu/address/testutil/TypicalEvents.java
@@ -0,0 +1,48 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.EventDate;
+import seedu.address.model.event.EventName;
+import seedu.address.model.event.EventTime;
+import seedu.address.model.person.PersonId;
+
+/**
+ * A utility class containing a list of {@code Event} objects to be used in tests.
+ */
+public class TypicalEvents {
+
+    public static final Event BIRTHDAY_PARTY = new EventBuilder().withName("Birthday Party")
+            .withDate("12-12-2025").withTime("18:00").build();
+    public static final Event MEETING = new EventBuilder().withName("Project Meeting")
+            .withDate("01-01-2024").withTime("10:00").build();
+    public static final Event CONFERENCE = new EventBuilder().withName("Annual Conference")
+            .withDate("20-03-2025").withTime("09:00").build();
+
+    // Manually added
+    public static final Event WORKSHOP = new EventBuilder().withName("Workshop")
+            .withDate("15-07-2024").withTime("14:00").build();
+    public static final Event LUNCH = new EventBuilder().withName("Team Lunch")
+            .withDate("05-02-2025").withTime("12:30").build();
+
+    private TypicalEvents() {} // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical events.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Event event : getTypicalEvents()) {
+            ab.addEvent(event);
+        }
+        return ab;
+    }
+
+    public static List<Event> getTypicalEvents() {
+        return new ArrayList<>(Arrays.asList(BIRTHDAY_PARTY, MEETING, CONFERENCE));
+    }
+}
