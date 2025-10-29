5cb98150d3ce4b48344797a7b867aaf1490f04ae
gabriel-lll <gabrielsim404@gmail.com>
2025-10-29 20:53:53 +0800

    Modify clear command to specify what to clear

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,17 +1,55 @@
 package seedu.address.logic.commands;
 
+import java.util.Objects;
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.Model;
 
 /**
- * Prompts the user for confirmation to clear the address book.
+ * Prompts the user for confirmation to clear parts of the address book.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " [all/parties/contacts]";
 
+    private static final Logger logger = LogsCenter.getLogger(ClearCommand.class);
+
+    private final ClearTarget target;
+
+    public ClearCommand() {
+        this(ClearTarget.ALL);
+    }
+
+    public ClearCommand(ClearTarget target) {
+        this.target = Objects.requireNonNullElse(target, ClearTarget.ALL);
+    }
+
+    // Getter for tests and external callers
+    public ClearTarget getTarget() {
+        return target;
+    }
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult("Are you sure you want to clear the party planner?", new ConfirmClearCommand());
+        Objects.requireNonNull(model);
+        logger.fine(() -> "Prepare to clear target: " + target);
+
+        String message;
+        switch (target) {
+        case CONTACTS:
+            message = "Are you sure you want to clear all contacts?";
+            break;
+        case EVENTS:
+            message = "Are you sure you want to clear all parties?";
+            break;
+        case ALL:
+        default:
+            message = "Are you sure you want to clear the party planner?";
+            break;
+        }
+
+        return new CommandResult(message, new ConfirmClearCommand(target));
     }
 }
