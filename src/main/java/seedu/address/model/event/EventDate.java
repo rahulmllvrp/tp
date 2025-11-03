2263a6c5a5a22e835bf11c93be81fe07c35e884b
Rahul Mallavarapu <98007910+rahulmllvrp@users.noreply.github.com>
2025-11-03 15:17:59 +0800

    Update EventDate.java

--- src/main/java/seedu/address/model/event/EventDate.java
+++ src/main/java/seedu/address/model/event/EventDate.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
